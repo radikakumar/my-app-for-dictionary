81a465cebc06da7a5d6452209d8a3f12a6bf4d2d
Radika <radikakumar@gmail.com>
2021-06-01 18:32:32 +0100

    Did API intergration for the dictionary by 1. installing axios 2. creating the url function to get response from axios 3. added variable keyword to the axios url then console.log the data to see the structure of the url and then added the dictionary app API reference.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, {useState}from "react"
-
+import axios from "axios";
 export default function Dictionary() {
 let [Keyword, setKeyword] = useState("");
 
 function handleSubmit(event){
     event.preventDefault();
-    alert(`Searching for ${Keyword}`);
+   
 }
 
 function updateChange (event){
 setKeyword(event.target.value);
+
+function handleResponse(response){
+    console.log(response.data[0]);
+}
+//https://dictionaryapi.dev/
+let url=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${Keyword}`;
+axios.get(url).then(handleResponse);
 }
     return (
     <div className="Dictionary">
